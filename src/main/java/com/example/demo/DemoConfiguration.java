5536a7c3857ae0ef7bb8e7b8b3b63e813f95f0c9
hongsj88 <hongsj88@gmail.com>
2024-09-05 07:33:29 +0900

    demoapplication

--- src/main/java/com/example/demo/DemoConfiguration.java
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.*;
+import org.springframework.batch.repeat.RepeatCallback;
+import org.springframework.batch.repeat.RepeatContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,27 +20,57 @@ import org.springframework.context.annotation.Configuration;
 public class DemoConfiguration {
 
 
-        private final JobBuilderFactory jobBuilderFactory;
-        private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
 
-        @Bean
-        public Job batchJob() throws Exception {
-            return jobBuilderFactory.get("batchJob")
-                    .incrementer(new RunIdIncrementer())
-                    .start(step1())
-                    .build();
-        }
+    @Bean
+    public Job batchJob() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
 
 
-        @Bean
-        public Step step1() throws Exception {
-            return stepBuilderFactory.get("step1")
-                    .<String, String>chunk(10)
-                    .reader(null)
-                    .writer(items-> System.out.println(items))
-                    .build();
-        }
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(5)
+                .reader(new ItemReader<String>() {
+                    int i = 0;
+
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        i++;
+                        return i > 3 ? null : "item" + i;
+
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+
+                    RepeatTemplate repeatTemplate = new RepeatTemplate();
+
+                    @Override
+                    public String process(String item) throws Exception {
+
+                        repeatTemplate.setCompletionPolicy(new SimpleCompletionPolicy(3));
+
+                        repeatTemplate.iterate(new RepeatCallback() {
+                            @Override
+                            public RepeatStatus doInIteration(RepeatContext repeatContext) throws Exception {
+
+                                System.out.println("repeatTemplate test");
+                                return RepeatStatus.CONTINUABLE;
+                            }
+                        });
+
+                        return item;
+                    }
+                })
+                .writer(items -> System.out.println(items))
+                .build();
+    }
 
 
 }
