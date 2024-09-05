45182a5419b5be3f4f82219795a88e624b24beda
hongsj88 <hongsj88@gmail.com>
2024-09-06 07:14:40 +0900

    repeattemplate

--- src/main/java/com/example/demo/DemoConfiguration.java
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -7,10 +7,15 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.*;
+import org.springframework.batch.repeat.CompletionPolicy;
 import org.springframework.batch.repeat.RepeatCallback;
 import org.springframework.batch.repeat.RepeatContext;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.exception.ExceptionHandler;
+import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
+import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
 import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,6 +48,7 @@ public class DemoConfiguration {
                     @Override
                     public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
                         i++;
+//                        System.out.println("read:::" + i);
                         return i > 3 ? null : "item" + i;
 
                     }
@@ -55,13 +61,24 @@ public class DemoConfiguration {
                     public String process(String item) throws Exception {
 
                         repeatTemplate.setCompletionPolicy(new SimpleCompletionPolicy(3));
+                        repeatTemplate.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
+
+                        CompositeCompletionPolicy completionPolicy = new CompositeCompletionPolicy();
+                        CompletionPolicy[] compositeCompletionPolicies = new CompletionPolicy[]{
+                                new SimpleCompletionPolicy(3),
+                                new TimeoutTerminationPolicy(3000)
+                        };
+                        completionPolicy.setPolicies(compositeCompletionPolicies);
+                        repeatTemplate.setCompletionPolicy(completionPolicy);
+
+                        repeatTemplate.setExceptionHandler(simpleLimitExceptionHandler());
 
                         repeatTemplate.iterate(new RepeatCallback() {
                             @Override
                             public RepeatStatus doInIteration(RepeatContext repeatContext) throws Exception {
-
-                                System.out.println("repeatTemplate test");
-                                return RepeatStatus.CONTINUABLE;
+                                System.out.println("repeatTemplate is testing");
+                                throw new RuntimeException("Exception is occurred");
+//                                return RepeatStatus.CONTINUABLE;
                             }
                         });
 
@@ -72,5 +89,9 @@ public class DemoConfiguration {
                 .build();
     }
 
+    @Bean
+    public ExceptionHandler simpleLimitExceptionHandler(){
+        return new SimpleLimitExceptionHandler(3);
+    }
 
 }
